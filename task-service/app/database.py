6ab8fa7a3c8c4cdd35fe73eba12582e78f8f9ce3
harish-112 <harishmunusami.work@gmail.com>
2026-05-28 12:52:20 +0530

    pvc file

--- task-service/app/database.py
+++ task-service/app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///./tasks.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./data/tasks.db")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
